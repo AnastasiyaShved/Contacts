9db141e95eadfc8ee5afa933e2f940b853044e53
AnastasiyaShved <pav159208@gmail.om>
2023-09-14 15:17:48 +0300

    реализован первый экран

--- Contacts/Contacts/Controllers/FirstVC.swift
+++ Contacts/Contacts/Controllers/FirstVC.swift
@@ -11,22 +11,27 @@ class FirstVC: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
-    
+    private var person = DataSource()
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
-
-        
     }
-
-
 }
-extension UIViewController: UITableViewDataSource {
-    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        10
+
+extension FirstVC: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return person.contactsList.count
     }
-    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "title", for: indexPath)
-    return cell
+    
+        let contact = person.contactsList[indexPath.row]
+        cell.textLabel?.text = contact
+    
+        return cell
+
     }
 }
--- Contacts/Contacts/DataServices/DataSource.swift
+++ Contacts/Contacts/DataServices/DataSource.swift
@@ -7,8 +7,11 @@
 
 import Foundation
 
+
+
 class DataSource {
-    var contacts: [String] = []
+
+    lazy var contactsList: [String] = fillingContactList()
     
     var names: [String] = ["name1",
                           "name2",
@@ -30,18 +33,18 @@ class DataSource {
                              " surName8",
                              " surName9",
                              " surName10"]
-    
-    private func fillingContactList () {
+
+    private func fillingContactList() -> [String] {
+        var result = [String]()
         for i in 0 ..< names.count {
-            let nameIndex = Int.random(in: 0...names.count-i)
-            let surNameIndex = Int.random(in: 0...surName.count-i)
-            contacts.append(names [nameIndex] + surName[nameIndex])
+            let nameIndex = Int.random(in: 0...max(names.count-i-1,0))
+            let surNameIndex = Int.random(in: 0...max(surName.count-i-1, 0))
+            result.append(names[nameIndex] + surName[surNameIndex])
             names.remove(at: nameIndex)
             surName.remove(at: surNameIndex)
         }
-   
+        return result
     }
-    
-    
-    
 }
+
+
